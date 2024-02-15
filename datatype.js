74b9bbd532f1b22d678f4b4134f1061de5f9afa4
samiuzzaman-chowdhury <chowdhurysamiuzzaman@gmail.com>
2024-02-15 22:47:46 +0600

    data type

---
+++ datatype.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 10.5;
+let c = "Hello";
+let d = 'World'
+let e = true;
+let g = null;
+let f = 
+
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);

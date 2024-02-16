e9f9b8d52172fa8bc32f40737564cf2a767748a6
samiuzzaman-chowdhury <chowdhurysamiuzzaman@gmail.com>
2024-02-17 01:23:48 +0600

    js variable

---
+++ 3.variables.js
@@ -0,0 +1,15 @@
+/* JS Variables Rules:
+
+1. A JavaScript variable is simply a name of storage location.
+
+2. Name must start a letter(a to z or A to Z), underscore(_), or dollar($)sign.
+
+3. After first letter we can use digits (0 to 9), for example value1.
+
+4. Javascript variables are case sansitive, for example x and X are different variables. 
+*/
+
+var x = 10;
+var y = 20;
+var z = x + y;
+document.write(z);

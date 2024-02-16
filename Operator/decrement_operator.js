b0cf80b8420df5303b590ba52d22cbb3b5efe9c8
samiuzzaman-chowdhury <chowdhurysamiuzzaman@gmail.com>
2024-02-16 13:21:10 +0600

    increment decrement operatir

---
+++ Operator/decrement_operator.js
@@ -0,0 +1,15 @@
+// let n = 10;
+// --n;
+
+// console.log(n);
+
+/* let n = 10
+// let m = 15 - n--;
+let m = 15 - --n;
+
+console.log(m);
+console.log(n); */
+
+let n = 5;
+console.log(n + " " + --n + " " + n--);
+console.log(n);
---
+++ Operator/increment_operator.js
@@ -0,0 +1,19 @@
+/* let n =5;
+
+n++ = Post Increment - Post Increment 1st value print then update value print.
+
+++n = Pre Increment - Pre Increment - 1st value update then value print */
+
+
+/* let n = 5;
+
+let m = 10 + n++;
+//let m = 10 + ++n;
+
+console.log(m);
+console.log(n); */
+
+
+let n = 5;
+console.log(n + " " + ++n + " " + n++);
+console.log(n);

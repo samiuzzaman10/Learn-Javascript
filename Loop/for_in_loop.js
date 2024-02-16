11832ed3c8c33dac77b28e83f3c9453ec59d4f18
samiuzzaman-chowdhury <chowdhurysamiuzzaman@gmail.com>
2024-02-16 18:41:20 +0600

    for_in & for_of

--- Loop/for_in_loop.js
+++ Loop/for_in_loop.js
@@ -1,6 +1,8 @@
+// I will use for_in it to get the index number
+
 const cname = "Stack Learner";
 
 for (let i in cname) {
-    //console.log(i);
-    console.log(cname[i]);
+  //console.log(i);
+  console.log(cname[i]);
 }
---
+++ Loop/for_of_loop.js
@@ -0,0 +1,8 @@
+// I will use for_of to get the value
+
+const cname = "Stack Learner";
+
+for (let v of cname) {
+  //console.log(i);
+  console.log(v);
+}

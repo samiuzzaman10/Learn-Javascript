c452bd2bec436b1f4ddac95c6f3735372d810259
samiuzzaman-chowdhury <chowdhurysamiuzzaman@gmail.com>
2024-02-17 21:09:52 +0600

    Traverse array elements

---
+++ 3. Array/3.traverse_array_elements.js
@@ -0,0 +1,40 @@
+const arr = [47, 5, 105, 78, 63, 99, 14, 55, 39];
+const n = arr[3];
+const m = arr[2];
+const x = 1,
+  y = 0;
+
+// console.log(m, n, arr[x], arr[y], arr[x] + arr[y], arr[x + y + 1]);
+
+// Simple Traverse
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// Array Sum & Avg
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+
+// console.log(sum / arr.length);
+
+// Find Large Number
+// let largestnumber = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//     if(arr[i] > largestnumber) {
+//         largestnumber = arr[i];
+//     }
+// }
+// console.log(largestnumber);
+
+// Find Small Number
+// let smallNumber = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] < smallNumber) {
+//     smallNumber = arr[i];
+//   }
+// }
+// console.log(smallNumber);
+
+

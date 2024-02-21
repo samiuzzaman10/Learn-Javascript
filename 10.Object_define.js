7d9ed05cac90a5fe11b85bb8ed17afe3dcaba49f
samiuzzaman-chowdhury <chowdhurysamiuzzaman@gmail.com>
2024-02-21 22:14:58 +0600

    Object Define

---
+++ 10.Object_define.js
@@ -0,0 +1,21 @@
+// Object is a key value pair store data
+
+// const o = {
+
+// }
+// console.log(typeof o);
+
+const person1 = {}
+person1.firstName = "Samiuzzaman";
+person1.lastName = "Chowdhury";
+person1.email = "chowdhurysamiuzzaman@gmail.com";
+console.log(person1);
+console.log(person1.email);
+
+const person2 = {
+    firstName : "Samiuzzaman",
+    lastName : "Chowdhury",
+    email : "chowdhurysamiuzzaman@gmail.com",
+}
+
+console.log(person2);

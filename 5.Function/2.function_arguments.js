f54ace939130537d1b815bf4c14e9d79dbfea600
samiuzzaman-chowdhury <chowdhurysamiuzzaman@gmail.com>
2024-02-22 00:50:03 +0600

    Function Arguments

---
+++ 5.Function/2.function_arguments.js
@@ -0,0 +1,17 @@
+/* 
+function myFunction(Parameters) {
+
+}
+myFunction(Arguments);
+*/ 
+
+
+function sum(a, b) {
+  const c = a + b;
+  console.log(c);
+}
+
+sum(10, 20);
+sum(100, 200);
+sum("Rahat", " Chowdhury");
+sum(10, "4");

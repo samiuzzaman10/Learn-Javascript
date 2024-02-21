ed53401030e360244cef4ed73b8a0b3b4cb136ae
samiuzzaman-chowdhury <chowdhurysamiuzzaman@gmail.com>
2024-02-22 01:16:12 +0600

    function return

---
+++ 5.Function/3.function_return.js
@@ -0,0 +1,16 @@
+// function addTwoNumber(n1, n2) {
+//   return "RAHA";
+// }
+// let newValue = addTwoNumber() + "T" + " Chowdhury";
+//console.log(newValue);
+
+function sum(a, b) {
+  const c = a + b;
+  return c;
+}
+
+const r1 = sum(10, 20);
+const r2 = sum(100, 200);
+const r3 = sum("Rahat", " Chowdhury");
+const r4 = sum(10, "4");
+console.log(r1, r2, r3, r4);
--- 5.Function/7.function_return.js
+++
@@ -1,5 +0,0 @@
-function addTwoNumber(n1, n2) {
-  return "RAHA";
-}
-var newValue = addTwoNumber() + "T" + " Chowdhury";
-document.write(newValue);

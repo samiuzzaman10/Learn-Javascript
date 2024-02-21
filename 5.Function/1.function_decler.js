42747098b69538165e6fff1d649331eab65fb41e
samiuzzaman-chowdhury <chowdhurysamiuzzaman@gmail.com>
2024-02-22 00:38:56 +0600

    Function Decler

---
+++ 5.Function/1.function_decler.js
@@ -0,0 +1,28 @@
+function myFunction() {
+  console.log("My First Function");
+  const a = 10;
+  const b = 20;
+  console.log("Sum = " + (a + b));
+}
+myFunction();
+myFunction();
+myFunction();
+myFunction();
+myFunction();
+
+
+
+// const arr1 = [4, 6, 10, 3, 29, 3, 23, 12];
+// const arr2 = [6, 11, 21, 32, 4, 3, 20, 10];
+
+// function findMax(arr) {
+//     let max = arr[0];
+//     for (let v of arr) {
+//         if (v > max) {
+//             max = v;
+//         }
+//     }
+//     return max;
+// }
+// console.log(findMax(arr1));
+// console.log(findMax(arr2));

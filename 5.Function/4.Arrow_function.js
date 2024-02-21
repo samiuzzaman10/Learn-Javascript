c5d0b0a0351f9be6759282272cd9a7f0c7bb55f6
samiuzzaman-chowdhury <chowdhurysamiuzzaman@gmail.com>
2024-02-22 01:30:57 +0600

    Arrow_function

---
+++ 5.Function/4.Arrow_function.js
@@ -0,0 +1,11 @@
+/*
+function Expression
+const sum = function (a, b) {
+  return a + b;
+};
+*/
+
+// Arrow Function
+const sum = (a, b) => a + b;
+const max = (a, b) => Math.max(a, b);
+console.log(max(10, 20));

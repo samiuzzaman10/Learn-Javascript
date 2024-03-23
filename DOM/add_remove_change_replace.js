0030f7a81c32cd2de44be422385af003988a3e12
samiuzzaman10 <chowdhurysamiuzzaman@gmail.com>
2024-03-23 23:30:39 +0600

    add remove change replace

---
+++ DOM/add_remove_change_replace.js
@@ -0,0 +1,23 @@
+// let heading2 = document.createElement("h2");
+// let text = document.createTextNode("My name is Rahat");
+// heading2.appendChild(text);
+
+// let myDiv = document.querySelector("#my-div");
+// myDiv.appendChild(heading2);
+
+// let heading1 = document.getElementsByTagName("h1")[1];
+// myDiv.removeChild(heading1);
+
+// let heading0 = document.createElement("h1");
+// let text0 = document.createTextNode("This is heading zero");
+// heading0.appendChild(text0);
+// myDiv.insertBefore(heading0, heading2);
+
+// console.log(myDiv);
+
+// let menu = document.querySelector("#menu");
+
+// let li = document.createElement("li");
+// li.textContent = "Home";
+
+// menu.replaceChild(li, menu.lastElementChild);

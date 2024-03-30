d8dcf2fe5ced7ba7348a21c8e7cff1448b8c55d8
Md Samiuzzaman Chowdhury <133011680+samiuzzaman10@users.noreply.github.com>
2024-03-30 23:23:38 +0600

    regular expression

---
+++ Regular_Expression.js
@@ -0,0 +1,12 @@
+// const text = '+88017912345678';
+// const regex = /(\+88)?-?01[1-9]\d{8}/g;
+
+// const matches = text.match(regex);
+
+// const index = text.search(regex);
+
+// const replaced = text.replace(regex, "phone");
+
+// const testing = regex.test(text);
+
+// console.log(matches, index, replaced, testing);
